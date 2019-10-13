d304a3af41ccd7ccec5708386c8c47469b145c12
SUBHAM <subham040800@gmail.com>
2019-10-13 12:58:04 +0530

    test commit

--- app/src/main/java/com/orange/flickrtask/ui/home/HomeFragment.java
+++ app/src/main/java/com/orange/flickrtask/ui/home/HomeFragment.java
@@ -59,7 +59,7 @@ public class HomeFragment extends Fragment {
         });
         return view;
     }
-    
+
     public void getRecentPhotos() {
         photoAdapter = new PhotoAdapter(getContext());
         homeViewModel.getNetworkStateLiveData().observe(this, new Observer<NetworkState>() {
@@ -98,6 +98,4 @@ public class HomeFragment extends Fragment {
         recyclerView.setAdapter(photoAdapter);
         photoAdapter.notifyDataSetChanged();
     }
-
-
 }
